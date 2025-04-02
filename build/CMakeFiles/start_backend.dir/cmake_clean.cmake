aede449278a038095039ca97184533ff2a9e7e7f
Jabezng2 <97502375+Jabezng2@users.noreply.github.com>
2025-04-03 02:02:18 +0800

    chore: untrack build folder

--- build/CMakeFiles/start_backend.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/start_backend"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/start_backend.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
